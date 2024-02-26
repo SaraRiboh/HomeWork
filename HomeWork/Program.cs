5405d346dc861870baa52931f56eb7f23d65ccd6
SaraRiboh <sr0583221328@gmail.com>
2024-02-26 13:39:36 +0200

    Add project files.

---
+++ HomeWork/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        class SwapList<T> : List<T>
+        {
+             public void SwapIndexes(int i,int ii,T z)
+            {             
+                z = this[i];
+                this[i] = this[ii];
+                this[ii]=z;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int obj = 3, i = 0, ii = 2;
+            double obj2 = 6.4;
+            string b= "";
+            static void PrintTheObj<T>(T obj)
+            {
+                Console.WriteLine(obj);
+            }
+
+            PrintTheObj(obj);
+           
+
+            PrintTheObj(obj2);
+
+
+
+            SwapList<string> s = new SwapList<string>() { "Dudi", "Chaim", "Shimi" };
+            s.SwapIndexes(i, ii, b);
+            for(int j = 0; j < s.Count; j++)
+            {
+                PrintTheObj(s[j]);
+            }
+           
+            
+            Console.Read();
+        } 
+
+    }
+}
